5546b75fb4259f194122fd3a7a10c5ec66f81d27
MEBonham <programmer@mckaybonham.com>
2021-12-28 07:05:11 -0700

    Might have fixed all bugs with moving items in inventory up and down?

--- zwolf/src/components/charSheets/ItemSummary.jsx
+++ zwolf/src/components/charSheets/ItemSummary.jsx
@@ -12,7 +12,7 @@ const ItemSummary = ({item}) => {
 
     useEffect(() => {
         menu.current.value = oldLoc;
-    }, []);
+    }, [cur]);
 
     const moveEquip = (ev) => {
         const newLoc = ev.target.value;
@@ -42,6 +42,54 @@ const ItemSummary = ({item}) => {
                         ]
                     });
                 }
+                break;
+            case "moveUp":
+                const beforeSlice = cur.equipment.slice(0, index);
+                let j = beforeSlice.length - 1;
+                while (j >= 0) {
+                    if (beforeSlice[j].depth === item.depth) break;
+                    j--;
+                }
+
+                setCur({
+                    ...cur,
+                    equipment: [
+                        ...cur.equipment.slice(0, j),
+                        {
+                            ...cur.equipment[index],
+                            i: j
+                        },
+                        ...cur.equipment.slice(j + 1, index),
+                        {
+                            ...cur.equipment[j],
+                            i: index
+                        },
+                        ...cur.equipment.slice(index + 1)
+                    ]
+                });
+                menu.current.value = oldLoc;
+                break;
+            case "moveDown":
+                const afterSlice = cur.equipment.slice(index + 1);
+                const k = afterSlice.findIndex((itemObj) => itemObj.depth === item.depth) + index + 1;
+
+                setCur({
+                    ...cur,
+                    equipment: [
+                        ...cur.equipment.slice(0, index),
+                        {
+                            ...cur.equipment[k],
+                            i: index
+                        },
+                        ...cur.equipment.slice(index + 1, k),
+                        {
+                            ...cur.equipment[index],
+                            i: k
+                        },
+                        ...cur.equipment.slice(k + 1)
+                    ]
+                });
+                menu.current.value = oldLoc;
                 break;
             default:
                 setCur({
@@ -76,6 +124,8 @@ const ItemSummary = ({item}) => {
                         <option key={itemObj.id} value={itemObj.id}>{itemObj.name}</option>
                     ))
                 }
+                {item.i > 0 ? <option value="moveUp">Move Up</option> : null}
+                {item.i + 1 < item.outOf ? <option value="moveDown">Move Down</option> : null}
                 <option value="sell">Sell (one)</option>
                 <option value="useUp">Use Up (one)</option>
             </select>
